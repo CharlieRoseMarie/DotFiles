2e293424088fe56792adbdb611448be2fab30550
Charlotte Allen <CharlotteRMAllen@gmail.com>
2019-12-12 14:09:35 -0800

    Add new follow keys to luakit

--- .config/luakit/userconf.lua
+++ .config/luakit/userconf.lua
@@ -1,1 +1,8 @@
 local settings = require "settings"
+
+local select = require "select"
+
+select.label_maker = function ()
+	local chars = charset("asdfqwerzxcv")
+        return trim(sort(reverse(chars)))
+end
